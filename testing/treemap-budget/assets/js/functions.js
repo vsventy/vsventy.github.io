f0fc2dc50f83deec594c45f225d4c987aacbc2ec
Vitalii Sventyi <svitaha@gmail.com>
2016-08-20 19:54:27 +0300

    add treemap-budget for testing

---
+++ testing/treemap-budget/assets/js/functions.js
@@ -0,0 +1,16 @@
+(function($) {
+  'use strict';
+
+  var TownBudget = {
+
+    // Initialization the functions
+    init: function() {
+
+    }
+  }
+
+  $(document).ready(function() {
+    TownBudget.init();
+  }); //end ready
+
+}(jQuery));
